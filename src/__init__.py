3840d8c3d6b8ac235b6b761fc119beae3097b41a
Ritam Biswas <136988968+gitritam06@users.noreply.github.com>
2026-04-24 00:35:06 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# src/__init__.py
+# Makes src/ a proper Python package so app.py can do:
+#   from src.legal import render_legal_section
+#   from src.faq   import render_faq
+#   from src.utils import get_data_with_fallback
